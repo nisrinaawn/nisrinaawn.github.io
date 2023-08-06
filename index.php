2e9de06a373a28b8be8b6953538e97ee5c227513
nisrinaawn <nwanadaa@gmail.com>
2023-08-06 11:28:20 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,279 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Viga&display=swap" rel="stylesheet">
+                    
+    <!-- Slick JS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css" integrity="sha512-yHknP1/AwR+yx26cB1y0cjvQUMvEa2PFzt1c9LlS4pRQ5NOTZFWbhBig+X9G9eYW/8m0/4OXNx8pxJ6z57x0dw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css" integrity="sha512-17EgCFERpgZKcm0j0fEq1YCJuyAWdz9KUtv1EjVuaOz8pDnh/0nZxmU6BBXwaaxqoi9PQXnRWqlcDB027hgv9A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+
+    <!-- My CSS -->
+    <link rel="stylesheet" href="style.css">
+    <title>Srie's Tailor</title>
+</head>
+
+<body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">Srie's Tailor</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav ml-auto">
+                <a class="nav-item nav-link active" href="#">Home<span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link" href="#about">About</a>
+                <a class="nav-item nav-link" href="#services">Services</a>
+                <a class="nav-item nav-link" href="#gallery">Gallery</a>
+                <button class="nav-item btn btn-custom tombol"><a href="#contact-us">Contact Us!</a></button>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <!-- End of Navbar -->
+
+    <!-- Jumbotron -->
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h1 class="display-4">Sew your clothes in <span>Smart</span><br> and <span>Easier</span> way with Us</h1>
+        </div>
+    </div>
+    <!-- End of Jumbotron -->
+
+
+<!-- Container -->
+<div class="container" >
+    <!-- Info Panel -->
+    <div class="row justify-content-center" >
+        <div class="col-10 info-panel">
+            <div class="row" id="about">
+                <div class="col-lg">
+                    <img src="img/employee.png" alt="sistem on" class="float-left">
+                    <h4>24 Hours</h4>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+                <div class="col-lg">
+                    <img src="img/baju.png" alt="baju" class="float-left">
+                    <h4>Comfortable to Wear</h4>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+                <div class="col-lg">
+                    <img src="img/security.png" alt="webservices" class="float-left">
+                    <h4>Easy Use</h4>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End of Info Panel -->
+
+    <!-- Working Space -->
+    <div class="row working-space">
+        <div class="col-lg-6">
+            <img src="img/hero-tailor.jpg" alt="working-space" class="img-fluid">
+        </div>
+        <div class="col-lg-5">
+            <h3>Get Your <span>Best Outfit</span> in a <span>Simple Way</span></h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, laudantium. Perspiciatis tempora eum quidem eius.</p>
+            <div class="row justify-content-center">
+                <button class="nav-item btn btn-custom tombol"><a href="#gallery">Gallery</a></button>
+            </div>
+        </div>
+    </div>
+    <!-- End of Working Space -->
+</div>
+<!-- End of Container -->
+
+<!-- Jasa -->
+<div class="jasa-wrapper" id="services">
+    <div class="container">
+        <section class="jasa">
+            <div class="judul">
+                <h1>Services</h1>
+            </div>
+
+            <div class="row res-slider">
+                <div class="col-12 mt-5 wrapper">
+                    <div class="row d-flex justify-content-center line">
+                        <div class="col-lg-4 d-flex">
+                            <img src="img/shirt.jpg" class="mx-auto my-auto" alt="">
+                        </div>
+                        <div class="col-lg-8 my-auto d-block teks">
+                            <h3>Formal's</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam rem asperiores autem est excepturi facilis, rerum illum cumque nostrum consequatur sed tempora iusto blanditiis cum provident unde officia in labore atque, mollitia quidem ipsa voluptas saepe! Distinctio ratione numquam odio!</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 mt-5 wrapper">
+                    <div class="row d-flex justify-content-center ">
+                        <div class="col-lg-4 d-flex ">
+                            <img src="img/uniform.png" class="mx-auto my-auto" alt="">
+                        </div>
+                        <div class="col-lg-8 my-auto d-block">
+                            <h3>Uniform</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam rem asperiores autem est excepturi facilis, rerum illum cumque nostrum consequatur sed tempora iusto blanditiis cum provident unde officia in labore atque, mollitia quidem ipsa voluptas saepe! Distinctio ratione numquam odio!</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 mt-5 wrapper">
+                    <div class="row d-flex justify-content-center ">
+                        <div class="col-lg-4 d-flex ">
+                            <img src="img/pants.jpg" class="mx-auto my-auto" alt="">
+                        </div>
+                        <div class="col-lg-8 my-auto d-block">
+                            <h3>Trousers</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam rem asperiores autem est excepturi facilis, rerum illum cumque nostrum consequatur sed tempora iusto blanditiis cum provident unde officia in labore atque, mollitia quidem ipsa voluptas saepe! Distinctio ratione numquam odio!</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 mt-5 wrapper">
+                    <div class="row d-flex justify-content-center ">
+                        <div class="col-lg-4 d-flex ">
+                            <img src="img/skirt.jpg" class="mx-auto my-auto" alt="">
+                        </div>
+                        <div class="col-lg-8 my-auto d-block">
+                            <h3>Skirt</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam rem asperiores autem est excepturi facilis, rerum illum cumque nostrum consequatur sed tempora iusto blanditiis cum provident unde officia in labore atque, mollitia quidem ipsa voluptas saepe! Distinctio ratione numquam odio!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</div>
+<!-- End of Jasa -->
+
+<!-- Gallery -->
+<section class="gallery" id="gallery">
+    <div class="container mx-auto">
+    <div class="judul">
+        <h1>Gallery</h1>
+        <p class="mx-auto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea perferendis quia laboriosam maxime itaque? Eos quibusdam rem fugit hic minima?</p>
+    </div>
+
+    <div class="row mt-5 mb-5 justify-content-center">
+        <img src="img/img2.png" alt="">
+        <img src="img/img2.png" alt="">
+        <img src="img/img2.png" alt="">
+        <img src="img/img2.png" alt="">
+        <img src="img/img2.png" alt="">
+        <img src="img/img2.png" alt="">
+        <img src="img/img2.png" alt="">
+        <img src="img/img2.png" alt="">
+        <img src="img/img2.png" alt="">
+    </div>
+    </div>
+</section>
+<!-- End of Gallery -->
+
+
+<!-- footer -->
+<div class="footer-wrapper" id="contact-us">
+    <div class="container">
+        <section class="footer">
+            <div class="row">
+                <div class="col-12 mt-5 mb-5 kolom-wrapper">
+                    <div class="row d-flex">
+                        <div class="col sosmed">
+                            <div class="row contact">
+                                <div class="col sosmed">
+                                    <h4>Contact Us</h4>
+                                    <a href="https://instagram.com"><i class="fa-brands fa-instagram"></i></a>
+                                    <a href="https://instagram.com"><i class="fa-brands fa-facebook"></i></a>
+                                    <a href="https://instagram.com"><i class="fa-brands fa-twitter"></i></a>
+                                    <a href="https://instagram.com"><i class="fa-brands fa-whatsapp"></i></a>
+                                </div>
+                            </div>
+                            <div class="row services mt-5">
+                                <div class="col services">
+                                    <h4>Services</h4> 
+                                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit, exercitationem?</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col alamat">
+                            <h4>Address</h4> 
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit, exercitationem?</>
+                        </div>
+                        <div class="col komen">
+                            <h4>Have Any Questions?</h4>
+                            <input type="email" placeholder="Type your email" class="input">
+                            <input type="email" placeholder="Type your question" class="input">
+                            <button class="komen btn btn-custom tombol"><a href="">Submit</a></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <div class="row copyright justify-content-center">
+                <p>All right reserved by <i class="fa-regular fa-copyright"></i> nisrinaawn</p>
+            </div>
+        </section>
+    </div>
+</div>
+<!-- End of Footer -->
+
+
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+
+    <!-- Script Carousel -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js" integrity="sha512-HGOnQO9+SP1V92SrtZfjqxxtLmVzqZpjFFekvzZVWoiASSQgSr4cw9Kqd2+l8Llp4Gm0G8GIFJ4ddwZilcdb8A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <script>
+        $('.res-slider').slick({
+            dots: false,
+            arrows: true,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            responsive: [{
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        infinite: true,
+                        dots: false
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+                // You can unslick at a given breakpoint now by adding:
+                // settings: "unslick"
+                // instead of a settings object
+            ]
+        });
+    </script>
+    </body>
+</html>
